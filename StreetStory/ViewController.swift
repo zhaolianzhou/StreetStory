23d96148fe1e5daf47cb2a126a2ea304a7f123be
zhaolianzhou <zhaolianzhou92@gmail,com>
2022-11-13 11:53:14 +1100

    Initial Commit

---
+++ StreetStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreetStory
+//
+//  Created by Zhaolian Zhou on 13/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
